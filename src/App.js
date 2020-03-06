f7f5c24026db732d4c6f56baec86a49edbe7b403
Christina Wu <CHRISTINASARAHWU@GMAIL.COM>
2020-03-06 15:51:07 -0800

    Title added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React , { Component, Fragment } from 'react';
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Title><h2>Curiosity Images</h2>
+        <p>Launched: 2011-11-26 | Landed: 2012-08-06 | Max Sol: 2540</p>
+      </Title>
+    </Fragment>
   );
 }
 
