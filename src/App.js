1848f3ef352f818b82908593949c60fe691259ab
Christina Wu <CHRISTINASARAHWU@GMAIL.COM>
2020-03-07 21:08:08 -0800

    Updated favicon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React , { Component, Fragment } from 'react';
-import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import Filters from "./components/Filters";
+import Wrapper from "./components/Wrapper";
+import CameraCards from "./components/CameraCards";
 import './App.css';
 
 function App() {
@@ -14,6 +15,9 @@ function App() {
       <Filters>
         <p>Cameras <span>Sol</span></p>
       </Filters>
+      <Wrapper>
+        <CameraCards></CameraCards>
+      </Wrapper>
     </Fragment>
   )
 }
--- src/components/CameraCards/CameraCards.js
+++ src/components/CameraCards/CameraCards.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./CameraCards.css";
 
-const CameraCards = props => <div className="filters">{props.children}</div>;
+const CameraCards = props => <div className="cards">{props.children}</div>;
 
 export default CameraCards;
