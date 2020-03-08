43fbed239e165c3da312d8943203004582eb4ac9
Christina Wu <CHRISTINASARAHWU@GMAIL.COM>
2020-03-07 22:11:19 -0800

    added api call

--- src/App.js
+++ src/App.js
@@ -5,21 +5,49 @@ import Wrapper from "./components/Wrapper";
 import CameraCards from "./components/CameraCards";
 import './App.css';
 
-function App() {
-  return (
-    <Fragment>
-      <Title>
-        <h2>Curiosity Images</h2>
-        <p>Launched: 2011-11-26 | Landed: 2012-08-06 | Max Sol: 2540</p>
-      </Title>
-      <Filters>
-        <p>Cameras <span>Sol</span></p>
-      </Filters>
-      <Wrapper>
-        <CameraCards></CameraCards>
-      </Wrapper>
-    </Fragment>
-  )
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=s4qQyX357rGwDgAa0XiIn7SArZdcbdJ3KviG9reH')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
+      });
+  }
+
+  render() {
+    var { isLoaded, items } = this.state;
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else
+    {
+      return (
+        <Fragment>
+        <Title>
+          <h2>Curiosity Images</h2>
+          <p>Launched: 2011-11-26 | Landed: 2012-08-06 | Max Sol: 2540</p>
+        </Title>
+        <Filters>
+          <p>Cameras <span>Sol</span></p>
+        </Filters>
+        <Wrapper>
+          <CameraCards></CameraCards>
+        </Wrapper>
+      </Fragment>
+      )
+    }
+  }
+
+};
 
 export default App;
--- src/components/CameraCards/CameraCards.js
+++ src/components/CameraCards/CameraCards.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./CameraCards.css";
 
-const CameraCards = props => <div className="cards">{props.children}</div>;
+const CameraCards = props => (
+    <div className="cards">
+        <div className="card-container">
+            <h3></h3>
+            <p>Photos: </p>
+            <p>Sample photo:</p>
+            <img alt={props} src={props} />
+        </div>
+    </div>
+);
 
 export default CameraCards;
