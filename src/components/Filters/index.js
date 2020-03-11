d08a1fe70b1723a95c0b0113c093d1c85f0a501b
Christina Wu <CHRISTINASARAHWU@GMAIL.COM>
2020-03-10 18:46:49 -0700

    got rid of duplicate files

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -1,1 +1,61 @@
-export {default} from "./Filters";
+import React from "react";
+import "./Filters.css";
+
+const Filters = props => 
+    <div className="filters">          
+        <p>Cameras <span className="solSpan">Sol</span></p>
+        <div className="optionsDiv">
+            <div className="dropdown">
+                <button className="camerasBTN"><span id="camerasSelected">7</span> Selected<i className="fa fa-caret-down" /></button>
+                <div className="dropdown-content">
+                    <div>
+                        <button className="dropDownBTN" id="selectAll">Select All</button>
+                        <button className="dropDownBTN" id="clear">Clear</button>
+                    </div>
+                    <div>
+                        <label className="container cameraLabel">
+                            <input type="checkbox" className="checkBox"/>
+                            <p>Front Hazard Avoidance Camera</p>
+                        </label>
+                    </div>
+                    <div>
+                        <label className="container cameraLabel">
+                            <input type="checkbox" className="checkBox" checked="checked" />
+                            <p>Navigation Camera</p>
+                        </label>
+                    </div>
+                    <div>
+                        <label className="container cameraLabel">
+                            <input type="checkbox" className="checkBox" checked="checked" />
+                            <p>Mast Camera</p>
+                        </label>
+                    </div>
+                    <div>
+                        <label className="container cameraLabel">
+                            <input type="checkbox" className="checkBox" checked="checked" />
+                            <p>Chemistry and Camera Complex</p>
+                        </label>
+                    </div>
+                    <div>
+                        <label className="container cameraLabel">
+                            <input type="checkbox" className="checkBox" checked="checked" />
+                            <p>Mars Hand Lens Imager</p>
+                        </label>
+                    </div>
+                    <div>
+                        <label className="container cameraLabel">
+                            <input type="checkbox" className="checkBox" checked="checked" />
+                            <p>Rear Hazard Avoidance Camera</p>
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <label className="container">
+                    <input className="solInput" type="number" placeholder="14" name="sol"/>
+                </label>
+            </div>
+        </div>
+    </div>;
+
+export default Filters;
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,1 +1,10 @@
-export {default} from "./Title";
+import React from "react";
+import "./Title.css";
+
+const Title = props => 
+    <div className="title">
+        <h2>Curiosity Images</h2>
+        <p>Launched: 2011-11-26 | Landed: 2012-08-06 | Max Sol: 2540</p>
+    </div>;
+
+export default Title;
